61edf17edb0dc3905960c63871f99b979be7c389
Nilothpal Kumar <Nilothpal.Kumar@godigit.com>
2024-09-18 17:25:00 +0530

    Modified in Branch

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
     public static void main(String[] args)
     {
         System.out.println("HELLO");
-        System.out.println("HI");
         System.out.println("HEY");
+
     }
 }
